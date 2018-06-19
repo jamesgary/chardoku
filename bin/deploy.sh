5ff6b72ff5a3752a8198f78a8b7183c4c8f61233
James Gary <mrjamesgary@gmail.com>
2018-06-19 01:01:23 -0700

    Update deploy.sh for github pages

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-aws s3 cp public s3://<YOUR BUCKET NAME HERE>.com/ --recursive --grants=read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+git checkout gh-pages
+git add public/js/main.js
+git commit -m "Deploy!"
+git push origin :gh-pages && git subtree push --prefix public origin gh-pages
+git checkout master
