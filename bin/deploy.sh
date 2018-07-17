b7586e039e35ea8ecb8b9d68204bc30338fa98ec
James Gary <mrjamesgary@gmail.com>
2018-07-16 21:58:03 -0700

    Update deploy script to actually work

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-git checkout gh-pages
-git add public/js/main.js
+echo "Make sure elm-live isn't running or it may override main.js while deploying!"
+
+# TODO optimize when 0.19 releases
+elm-make src/Main.elm --output=public/js/main.js
+
+# make fresh gh-pages branch
+git branch -D gh-pages
+git checkout --orphan gh-pages
+git add -f public/js/main.js
 git commit -m "Deploy!"
 git push origin :gh-pages && git subtree push --prefix public origin gh-pages
 git checkout master
